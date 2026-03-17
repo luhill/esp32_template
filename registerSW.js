a81d0fce657f5a53051836a232267196db1eb5c7
luhill <freeflyhill@gmail.com>
2026-03-17 17:33:04 +1000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/esp32_template/sw.js', { scope: '/esp32_template/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
